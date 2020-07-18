2deb6e72b0b832c0225f610e83633a12eb079895
chages16 <chages16@gmail.com>
2020-07-18 22:54:42 +0930

    Here we go

--- index.js
+++ index.js
@@ -4,35 +4,24 @@ const questions = [
     "Include a Description here",
     "How do you install the system? Please provide a step by step description!",
     "Please provide instructions on how to use the application",
-    "What is the lisence name?",
+    "What is the license name?",
     "Please enter github usernames of any other contributors",
     "Provide examples on how to run tests.",
     "Please enter any questions/ bugs/ next steps?",
     "What is your name?",
-    "What is your github URL?",
+    "What is your personal github URL?",
+    "Enter the destination to the picture of your screenshot (e.g: 'assets/screenshot.jpg')",
+    "Enter the destination to your application (e.g: www.user.chage/application/')",
+    "Enter the destination to your repository",
 ];
 
-
-
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
 function init() {
-    var inquirer = require('inquirer');
-    var fs = require('fs');
-    var path = require('path');
+    const inquirer = require('inquirer');
+    const fs = require('fs');
+    const path = require('path');
     const userResponse = inquirer
 
         .prompt([
-
-            {
-                type: "confirm",
-                message: "Welcome to the great readme generator, are you ready to begin?",
-                name: "confirming"
-            },
 
             {
                 type: "input",
@@ -84,8 +73,34 @@ function init() {
                 message: questions[9],
                 name: "url"
             },
+            {
+                type: "input",
+                message: questions[10],
+                name: "imageurl"
+            },
+            {
+                type: "input",
+                message: questions[11],
+                name: "appurl"
+            },
+            {
+                type: "input",
+                message: questions[12],
+                name: "repourl"
+            },
+            {
+                type: "confirm",
+                message: "Are you happy with your answers?",
+                name: "confirming"
+            },
+
         ])
         .then(userResponse => {
+            const resConfirming = userResponse.confirming
+            if (resConfirming == false) {
+                console.log("Please start again if you want")
+                return;
+            }
             const resTitle = userResponse.title;
             const resDescription = userResponse.description;
             const resInstallation = userResponse.installation;
@@ -96,36 +111,42 @@ function init() {
             const resQuestions = userResponse.questions;
             const resAuthor = userResponse.author;
             const resURL = userResponse.url;
-            console.log(resTitle);
-            console.log("done")
-            var readmeOutput = (`
-            # ${resTitle} 
-            ${resDescription}
+            const resIMGURL = userResponse.imageurl;
+            const resAppURL = userResponse.appurl;
+            const resRepoURL = userResponse.repourl; 
+            console.log("Generating Readme")
+            var readmeOutput = (`# ${resTitle} 
+            \n ${resDescription}
+            \n<img src="${resIMGURL}" width="500"  alt="Picture"/>
+            \n ## Contents
             \n* [Installation](#Installation)
             \n* [Usage](#Usage)
             \n* [License](#License)
             \n* [Contributors](#Contributors)
             \n* [Tests](#Tests)
+            \n* [Links](#Links)
             \n* [Author](#Author)
-            ## Installation
-            ${resInstallation}
-            ## Usage
-            ${resUsage}
-            \`\`\`
-            ## License 
-            This project is licensed under the ${resLicense} - see ${"licenseUrl"} for more details
-            ## Contributors
-            ${resContributing}
-            ## Tests
-            ${resTests}
-            ## Author 
-            ${resAuthor}
-            \nGitHub: ${resURL}
+            \n ## Installation
+            \n ${resInstallation}
+            \n ## Usage
+            \n ${resUsage}
+            \n ## License 
+            \n This project is licensed under the ${resLicense}
+            \n ## Contributors
+            \n ${resContributing}
+            \n ## Tests
+            \n ${resTests}
+            \n ## Links
+            \n [${resTitle} Application](${resAppURL})
+            \n [${resTitle} Repository](${resRepoURL})
+            \n ## Author 
+            \n [${resAuthor}](${resURL})
             `)
-            fs.writeFile('readMegen.md', readmeOutput, function (err) {
+            fs.writeFile('generatedreadme/readme.md', readmeOutput, function (err) {
                 if (err) throw err;
-                console.log('File is created successfully.');
+                console.log('File is created successfully');
             });
+
         })
 
 }
